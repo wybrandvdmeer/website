2a1daf8157c261ac0894dc801fc662dae7d95f0b
Wybrand van der Meer <wybrandvdmeer@gmail.com>
2017-02-09 11:13:41 +0100

    Vars are stored in the session storage.

--- scripts/functionality.js
+++ scripts/functionality.js
@@ -1,13 +1,34 @@
-var slideIndex = 1;
-var showContent = true;
+showSlides(0);
+showContent();
 
-function hideContent() {
+function getSlideIndex() {
+  if(sessionStorage.slideIndex === undefined) {
+    sessionStorage.slideIndex = 1;
+  }
+  return parseInt(sessionStorage.slideIndex);
+}
+
+function setSlideIndex(slideIndex) {
+  sessionStorage.slideIndex = slideIndex;
+}
+
+function getShowContent() {
+  if(sessionStorage.showContent === undefined) {
+    sessionStorage.showContent = true;
+  }
+  return sessionStorage.showContent === 'true';
+}
+
+function setShowContent(showContent) {
+  sessionStorage.showContent = showContent;
+}
+
+function showContent() {
     var content = document.getElementsByClassName("content")[0];
     var up = document.getElementsByClassName("button-up")[0];
     var down = document.getElementsByClassName("button-down")[0];
+    var showContent = getShowContent();
    
-    showContent = !showContent;
-
     if(showContent) {
       content.style.display = "block";
       down.style.display = "block";
@@ -19,21 +40,26 @@ function hideContent() {
     }
 }
 
-showSlides(slideIndex);
+function hideContent() {
+    var shwContent = getShowContent();
+    setShowContent(!shwContent);
+    showContent();
+}
 
 function nextSlide() {
-  slideIndex++;
-  showSlides(); 
+  showSlides(1); 
 }
 
 function previousSlide() {
-  slideIndex--;
-  showSlides(); 
+  showSlides(-1); 
 }
 
-function showSlides() {
-  var i;
+function showSlides(delta) {
   var slides = document.getElementsByClassName("slide");
+
+  var slideIndex = getSlideIndex();
+
+  slideIndex += delta;
   
   if (slideIndex > slides.length) {
     slideIndex = 1;
@@ -43,9 +69,11 @@ function showSlides() {
     slideIndex = slides.length;
   }
 
-  for (i = 0; i < slides.length; i++) {
+  for (var i = 0; i < slides.length; i++) {
       slides[i].style.display = "none"; 
   }
 
   slides[slideIndex - 1].style.display = "block";
+
+  setSlideIndex(slideIndex);
 }
