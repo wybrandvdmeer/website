55111d9deb3a5ec0ae8ca71b08b09f65b9e0a52d
Wybrand van der Meer <wybrandvdmeer@gmail.com>
2017-02-10 10:08:24 +0100

    All content (including arrows) must be on the slide-show container.

---
+++ contact/scripts/contact.php
@@ -0,0 +1,16 @@
+<?php 
+if(isset($_POST['submit'])){
+    $to = "wybrandvdmeer@gmail.com"; // this is your Email address
+    $from = $_POST['email']; // this is the sender's Email address
+    $name = $_POST['name'];
+    $subject = "Form submission";
+    $message = $name . " " . " wrote the following:" . "\n\n" . $_POST['message'];
+
+    $headers = "From:" . $from;
+    $headers2 = "From:" . $to;
+    mail($to,$subject,$message,$headers);
+    echo "Mail Sent. Thank you " . $name . ", we will contact you shortly.";
+    // You can also use header('Location: thank_you.php'); to redirect to another page.
+    // You cannot use header and echo together. It's one or the other.
+    }
+?>
