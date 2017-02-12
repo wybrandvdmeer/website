7d1c3e933857608e4401a8e07c9f1ee120110a84
Wybrand van der Meer <wybrandvdmeer@gmail.com>
2017-02-12 14:32:39 +0100

    Using now 123contactform.com for mail.

--- contact/scripts/functionality.js
+++ contact/scripts/functionality.js
@@ -9,33 +9,3 @@ function hideEmailBox() {
 	emailBox.style.display = 'none';
 	showCurrentBackGround();
 }
-
-function triggerArrow() {
-	emailBoxArrow = document.getElementsByClassName("mail-box-arrow")[0];
-	emailBoxArrow.style.display = 'block';
-	
-	addListener(emailBoxArrow);
-	toggleClass(emailBoxArrow, 'send-mail', 'add');
-}
-
-function toggleClass(element, clzz, action) {
-	var classes = element.className;
-	var regex = new RegExp('\\b' + clzz + '\\b');
-	var hasOne = classes.match(regex);
-	clzz = clzz.replace(/\s+/g, '');
-	if(action === 'add' && !hasOne) {
-    	element.className = classes + clzz;
-	}
-	if(action === 'remove' && hasOne) {
-    	element.className = classes.replace(regex, '');
-	}
-}
-
-function addListener(element) {
-	element.addEventListener('webkitAnimationEnd', function() {
-		toggleClass(this, 'send-mail', 'remove');
-	});
-	element.addEventListener('animationend', function() {
-		toggleClass(this, 'send-mail', 'remove');
-	});
-}
